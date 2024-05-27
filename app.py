d566013c26071392abb699342845e62e5b643429
n19ht <andrewmeier222@gmail.com>
2024-05-26 22:05:53 -0400

    Updating to add additional app function

--- app.py
+++ app.py
@@ -46,6 +46,64 @@ bank_checking = 0
 bank_level = 0
 bank_items = []
 
-## ToDo
 ## Track what in your hands
-## Likely something we will expand in the future
+left_hand = None
+right_hand = None
+
+
+
+decision_request = """
+Please decide on what you are doing here?
+
+w - work
+
+
+"""
+
+
+print(decision_request)
+
+u_input = input('')
+
+
+if u_input == 'w':
+    print('You selected to do some work.')
+
+    work_request_text = '''
+What work are you trying to do right now?
+
+f - farm
+w - woodworking
+c - cooking
+m - mining
+x - firstaid
+
+'''
+    print(work_request_text)
+
+    w_request = input('')
+    ## This is where we decide on what type of farming we are doing today.
+
+    if w_request == 'f':
+        print(' We are farming!')
+
+        type_farm_request_text = '''
+What work are you trying to do right now?
+
+w - wheat
+r - rice
+c - corn
+
+'''
+        print(type_farm_request_text)
+        type_farm_request = input('')
+        
+        if type_farm_request == 'w':
+            print('You chose to farm wheat.')
+        elif type_farm_request == 'r':
+            print('You chose to farm rice.')
+        elif type_farm_request == 'c':
+            print('You chose to farm corn.')
+
+
+       
