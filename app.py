d03e81435683b8aa05cdfdd60ec417c8766e2488
n19ht <andrewmeier222@gmail.com>
2024-05-26 21:20:42 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+
+
+
+## Define Character Stats
+character_level = None
+experience_points = None
+
+
+# talents
+
+hp = None
+strength = None
+
+## Skills
+
+woodworking = 0
+farming = 0
+cooking = 0
+mining = 0
+firstaid = 0
+
+
+## Tracking from last session
+
+current_day = None
+current_xp = 0
+
+
+
+## Track whats in your pockets
+
+front_pockets = []
+back_pockets = []
+
+
+## Track whats in your backpack
+
+backpack_small = []
+backpack_medium = []
+backpack_large = []
+
+
+## Track what in your bank
+
+bank_checking = 0
+bank_level = 0
+bank_items = []
+
+## ToDo
+## Track what in your hands
+## Likely something we will expand in the future
