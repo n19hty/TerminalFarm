9baa5e8142c5c2f1b1ceaeec5c9cd876270576dc
n19hty <andrewmeier222@gmail.com>
2024-07-21 22:57:16 -0400

    added a few more variables to the game play and a counter for days planted

--- main.py
+++ main.py
@@ -7,35 +7,55 @@ from datetime import datetime
 print("Welcome to Terminal Farm.")
 
 
+#Set up for the game
+
+current_game_day = 0
+
+#Set up the 4 plots 2x2
+
+a1_planted = False
+a1_seed = "none"
+a1_time_planted = "xx:xx:xx"
+a1_days_planted = 0
+
+a2_planted = False
+a2_seed = "none"
+a2_time_planted = "xx:xx:xx"
+a2_days_planted = 0
+
+b1_planted = False
+b1_seed = "none"
+b1_time_planted = "xx:xx:xx"
+b1_days_planted = 0
+
+b2_planted = False
+b1_seed = "none"
+b2_time_planted = "xx:xx:xx"
+b2_days_planted = 0
+
+
+
+
+
+
 def configure_plot():
     pass
 
 
 def plant():
 
-    a1_planted = False
-    a1_seed = ""
-    a1_time_planted = ""
-    
-    a2_planted = False
-    a2_seed = ""
-    a2_time_planted = ""
-    
-    b1_planted = False
-    b1_seed = ""
-    b1_time_planted = ""
-    
-    b2_planted = False
-    b1_seed = ""
-    b2_time_planted = ""
-    
-    
+        
     #ask the user which plot they would like to plant
     plant_location = input("Where would you like to plant? Select a1, a2, b1, b2 : ")
 
 
     # create the if choices here
     if plant_location == "a1":
+        global a1_planted
+        global a1_seed
+        global a1_time_planted
+
+
         print("Great you are planting in " + plant_location)
         now = datetime.now().strftime("%H:%M:%S")
         a1_planted = True
@@ -58,11 +78,13 @@ def plant():
         print("You've entered something wrong please check your input and try again. (must be a1, a2, b1, or b2)")
 
 
+def print_plots():
+
     print("This is what the current plots look like: ")
-    print("A1 Planted: ", a1_planted, "A1 Time Planted: ", a1_time_planted, "A1 Seed: ", a1_seed)
-    print("A2 Planted: ", a2_planted, "A2 Time Planted: ", a2_time_planted, "A2 Seed: ", a2_seed)
-    print("B1 Planted: ", b1_planted, "B1 Time Planted: ", b1_time_planted, "B1 Seed: ", b1_seed)
-    print("B2 Planted: ", b2_planted, "B2 Time Planted: ", b2_time_planted, "B2 Seed: ", b1_seed)
+    print("A1 Planted:", a1_planted, "A1 Time Planted:", a1_time_planted, "A1 Seed:", a1_seed)
+    print("A2 Planted:", a2_planted, "A2 Time Planted:", a2_time_planted, "A2 Seed:", a2_seed)
+    print("B1 Planted:", b1_planted, "B1 Time Planted:", b1_time_planted, "B1 Seed:", b1_seed)
+    print("B2 Planted:", b2_planted, "B2 Time Planted:", b2_time_planted, "B2 Seed:", b1_seed)
 
 
 
@@ -73,7 +95,7 @@ def main():
         print("""
               1.Plant
               2.Water
-              3.Not Sure Yet
+              3.Print Plots
               4.Exit/Quit
               """)
         ans = input("What would you like to do? ")
@@ -83,7 +105,8 @@ def main():
         elif ans == "2":
            print("\n Student Deleted")
         elif ans == "3":
-           print("\n Student Record Found")
+           print("\n Printing Plots...")
+           print_plots()
         elif ans == "4":
            print("\n Goodbye")
         elif ans != "":
